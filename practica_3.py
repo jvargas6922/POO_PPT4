99d05aeb3f32da9c054fe583990b0948d526c058
adrian6922 <vargaszambranoj@gmaail.com>
2025-12-29 20:38:52 -0300

    practica 3

---
+++ practica_3.py
@@ -0,0 +1,32 @@
+"""
+Portal inmuliario nos contrata para poder crear inmuebles en un su portal web.
+Consigna tu como desarrollador debes permitrile al usuario poder crear inmuebles y luego mostrar la informacion de los mismos.
+- cosas a tener en cuenta debes validar la entrada de datos y la llamada de los metodos propios de cada clase.
+
+clase base para Inmuebles
+    Atributos:
+        - direccion: str
+        - metros_cuadrados: float
+        - precio: float
+
+    info_propiedad()->str
+
+clase Casa
+    Atributos:
+        - tipo_casa: str
+        - cantidad_habitaciones: int
+        - cantidad_banio: int
+        - patio: bool
+
+    info_propiedad()->str
+    ir_jardin()->str
+
+clase Departamento
+    Atributos:
+        - piso: int
+        - bodega: bool
+        - estacionamiento: bool
+    info_propiedad()->str
+    ir_terraza()->str
+
+"""
